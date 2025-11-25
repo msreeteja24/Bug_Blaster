a2e4ce87d366dfb4c3b15077df6bc28ee623eaae
Sree Teja <msreeteja24@gmail.com>
2025-11-25 12:33:17 +0530

    Addind the actions inside the reducer

--- src/reducers/ticketReducer.js
+++ src/reducers/ticketReducer.js
@@ -3,7 +3,21 @@
 export default function tikcetReducer(state, action) {
   switch (action.type) {
     case "ADD_TICKET":
-      return;
+      return { ...state, tickets: [...state.tickets, action.payload] };
+    case "UPDATE_TICKET":
+      return {
+        ...state,
+        tickets: state.tickets.map((ticket) =>
+          ticket.id === action.payload.id ? action.payload : ticket
+        ),
+      };
+    case "DELETE_TICKET":
+      return {
+        ...state,
+        tickets: state.tickets.filter(
+          (ticket) => ticket.id !== action.payload.id
+        ),
+      };
     default:
       return state;
   }
