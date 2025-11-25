b9ebbaeb170f5b0f9508bc0758b5f72c0537ce22
Sree Teja <msreeteja24@gmail.com>
2025-11-25 12:50:06 +0530

    Registering the ticketReducer in App.js and  dispatching the Add ticket action using reducer hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import "./styles.css";
+import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/ticketReducer";
 
 function App() {
+  const initialstate = { tickets: [] };
+  const [state, dispatch] = useReducer(ticketReducer, initialstate);
+
   return (
     <div className="App">
       <div className="container">
         <h1>Bug Blaster</h1>
-        <TicketForm></TicketForm>
+        <TicketForm dispatch={dispatch}></TicketForm>
       </div>
     </div>
   );
